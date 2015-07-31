57cd4c312182f965d8f51d969b513d49610c96ad
ctiux2 <ctiux2@MAC-C02N9BZUG3QC.Corp.ad.ctc>
2015-07-30 20:41:16 -0400

    issue/1

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bmistApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bmistApp
+ */
+angular.module('bmistApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bmistApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bmistApp
+ */
+angular.module('bmistApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
